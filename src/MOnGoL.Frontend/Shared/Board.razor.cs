14e95b1dc81227c2fd2d0310255612892d47a906
Bart de Boer <bart.de.boer@divverence.com>
2020-12-14 14:23:17 +0100

    Move also TokenStock out of Board

--- src/MOnGoL.Frontend/Shared/Board.razor.cs
+++ src/MOnGoL.Frontend/Shared/Board.razor.cs
@@ -14,7 +14,6 @@ namespace MOnGoL.Frontend.Shared
     public partial class Board : IDisposable
     {
         [Inject] private IPlayerService PlayerService { get; set; }
-        [Inject] private ILogger<PlayerList> Logger { get; set; }
 
         private Common.Board? board;
 
@@ -23,16 +22,11 @@ namespace MOnGoL.Frontend.Shared
 
         private PlacedToken? Coor(Coordinate coordinate) => board?.TokenAt(coordinate);
 
-        private int Countdown { get; set; }
-
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
             PlayerService.OnBoardChanged += OnBoardChanged;
-            PlayerService.OnCountdownChanged += OnCountdownChanged;
-            PlayerService.OnTokenStockChanged += OnTokenStockChanged;
             board = await PlayerService.GetBoard();
-            await SetTokenStock(await PlayerService.GetTokenStock());
         }
 
         private async Task Put(Coordinate coor)
@@ -46,34 +40,11 @@ namespace MOnGoL.Frontend.Shared
             await InvokeAsync(StateHasChanged);
         }
 
-        private async void OnCountdownChanged(object sender, int countDown)
-        {
-            Countdown = countDown;
-            await InvokeAsync(StateHasChanged);
-        }
-
-        private async void OnTokenStockChanged(object sender, int newStockValue)
-        {
-            await SetTokenStock(newStockValue);
-        }
-
-        private async Task SetTokenStock(int newStockValue)
-        {
-            var myInfo = await PlayerService.GetMyInfo();
-            TokenStock = myInfo is null
-                ? Array.Empty<string>()
-                : Enumerable.Repeat(myInfo.Token.Emoji, newStockValue).ToArray();
-            await InvokeAsync(StateHasChanged);
-        }
-
         public void Dispose()
         {
             PlayerService.OnBoardChanged -= OnBoardChanged;
-            PlayerService.OnCountdownChanged -= OnCountdownChanged;
-            PlayerService.OnTokenStockChanged -= OnTokenStockChanged;
         }
 
         public static string DummyEmoji { get; } = "╳\uFE0F";
-        public string[] TokenStock { get; private set; } = Array.Empty<string>();
     }
 }
---
+++ src/MOnGoL.Frontend/Shared/TokenStock.razor.cs
@@ -0,0 +1,46 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using MOnGoL.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOnGoL.Frontend.Shared
+{
+    public partial class TokenStock : IDisposable
+    {
+        [Inject] private IPlayerService PlayerService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            PlayerService.OnTokenStockChanged += OnTokenStockChanged;
+            await SetTokenStock(await PlayerService.GetTokenStock());
+        }
+
+        private async void OnTokenStockChanged(object sender, int newStockValue)
+        {
+            await SetTokenStock(newStockValue);
+        }
+
+        private async Task SetTokenStock(int newStockValue)
+        {
+            var myInfo = await PlayerService.GetMyInfo();
+            Stock = myInfo is null
+                ? Array.Empty<string>()
+                : Enumerable.Repeat(myInfo.Token.Emoji, newStockValue).ToArray();
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            PlayerService.OnTokenStockChanged -= OnTokenStockChanged;
+        }
+
+        public string[] Stock { get; private set; } = Array.Empty<string>();
+    }
+}
