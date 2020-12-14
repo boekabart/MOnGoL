c8cb535e6fe233c0cc6e8c0dbbc3452c2d8353d0
Bart de Boer <bart.de.boer@divverence.com>
2020-12-14 13:05:54 +0100

    Extracted Countdown out of Board for performance reasons

---
+++ src/MOnGoL.Frontend/Shared/Countdown.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using MOnGoL.Common;
+using System;
+using System.Threading.Tasks;
+
+namespace MOnGoL.Frontend.Shared
+{
+    public partial class Countdown : IDisposable
+    {
+        [Inject] private IPlayerService PlayerService { get; set; }
+
+        private int CountdownCount { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            PlayerService.OnCountdownChanged += OnCountdownChanged;
+        }
+
+        private async void OnCountdownChanged(object sender, int countDown)
+        {
+            CountdownCount = countDown;
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            PlayerService.OnCountdownChanged -= OnCountdownChanged;
+        }
+    }
+}
